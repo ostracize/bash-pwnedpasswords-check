df530e619801e5ee59f19d65186f4d9337e30c1b
ostracize <wbeldman+github@gmail.com>
2018-02-22 14:04:24 -0500

    Initial load

---
+++ hibp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "What password do you want to check? " PASS
+echo "Checking:" $PASS
+
+HASH=`echo -n ${PASS} | /usr/bin/shasum | awk '{print $1}'`
+PREFIX=`echo -n ${HASH} | cut -c1-5`
+SUFFIX=`echo -n ${HASH} | cut -c6-99`
+
+RESULT=`curl -s -o - https://api.pwnedpasswords.com/range/${PREFIX} | grep -i ${SUFFIX}`
+
+if [ $? != 0  ]; then
+    echo "Password not found";
+else
+    COUNT=`echo ${RESULT} | awk -F: '{print $2}'`;
+    echo "Password count: ${COUNT}"
+fi;
