c53d2b81fc7387a0d2f0242b28c3e3800950fa31
ostracize <wbeldman+github@gmail.com>
2018-02-22 14:15:37 -0500

    Cleaned up and added some comments

--- hibp.sh
+++ hibp.sh
@@ -2,10 +2,12 @@
 read -p "What password do you want to check? " PASS
 echo "Checking:" $PASS
 
+# Split the SHA1 hash of the password
 HASH=`echo -n ${PASS} | /usr/bin/shasum | awk '{print $1}'`
 PREFIX=`echo -n ${HASH} | cut -c1-5`
 SUFFIX=`echo -n ${HASH} | cut -c6-99`
 
+# https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange
 RESULT=`curl -s -o - https://api.pwnedpasswords.com/range/${PREFIX} | grep -i ${SUFFIX}`
 
 if [ $? != 0  ]; then
